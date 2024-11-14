a8e9bc87319b7240f7dd8ca18d8d11843eb13e92
Thanh035 <89309064+Thanh035@users.noreply.github.com>
2024-11-14 19:52:14 +0700

    results

--- src/main/kotlin/com/exmaple/demo/DemoApplication.kt
+++ src/main/kotlin/com/exmaple/demo/DemoApplication.kt
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:src/main/kotlin/com/mycompany/myapp/DemoApplication.kt
-package com.mycompany.myapp
-=======
+
 package com.exmaple.demo
->>>>>>> quangthanh/dev:src/main/kotlin/com/exmaple/demo/DemoApplication.kt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
